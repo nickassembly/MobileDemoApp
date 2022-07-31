c752a4edd9e79e635224eb41bc9ce10cdfae9681
nickguerra <nickg@maliciousgames.com>
2022-07-31 08:23:41 -0500

    initial commit

---
+++ MobileAppDemo/MobileAppDemo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MobileAppDemo.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MobileAppDemo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
